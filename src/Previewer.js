10b8abcf7fc4ec443167a7a81946925b0f7e0b7e
osycon <fredrichawk@gmail.com>
2017-04-29 10:45:16 +0200

    Change background to white and replaced textarea with MUI TextField

--- src/Previewer.js
+++ src/Previewer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import marked from 'marked';
+import TextField from 'material-ui/TextField';
 import Showcase from './Showcase';
 
 class Previewer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: `Write some markdown here.`
+      result: ``
     }
     this.handleUpdate = this.handleUpdate.bind(this);
   }
@@ -20,7 +21,13 @@ class Previewer extends Component {
       <div>
         <section className="previewer">
           <article className="input">
-            <textarea name="input" onChange={this.handleUpdate} id="text-input" cols="30" rows="10" defaultValue={this.state.result} />
+            <TextField
+              floatingLabelText="Write some markdown here."
+              rows={2}
+              rowsMax={30}
+              onChange={this.handleUpdate}
+              id="text-input"
+            />
           </article>
           <article className="showcase">
             <Showcase output={this.state.result} />
