ef59838026035dde1694d12e79141534b54b6ed7
osycon <fredrichawk@gmail.com>
2017-04-29 10:31:25 +0200

    Add Icon for markdown previewer

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import { blue50 } from 'material-ui/styles/colors';
+
+const iconStyles = {
+  marginRight: 24,
+  marginTop: 12,
+  paddingLeft: 15
+};
 
 const Header = () => {
   return (
-    <AppBar title="Markdown Previewer" />
+    <AppBar
+      title="Markdown Previewer"
+      iconElementLeft={<ActionHome style={iconStyles} color={blue50} />}
+    />
   );
 };
 
