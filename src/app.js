31500d9820598d4bdab2a592d8c979b7b101e02d
osycon <fredrichawk@gmail.com>
2017-05-01 09:14:00 +0200

    Change material theme color

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+  deepPurple300,
+  deepPurple500,
+  deepPurple700
+} from 'material-ui/styles/colors';
 import css from './style.scss';
 import Header from './Header';
 import Previewer from './Previewer';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: deepPurple500,
+    primary2Color: deepPurple300,
+    primary3Color: deepPurple700
+  }
+});
+
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <div>
       <Header />
       <Previewer />
